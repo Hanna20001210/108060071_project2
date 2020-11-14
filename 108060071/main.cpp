e0317f36a3b72a1184e8ef5262ed874505d8e5a5
“Hanna20001210” <hanna70343@gmail.com>
2020-11-14 20:03:36 +0800

    Initial Commit

---
+++ 108060071/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  108060071
+//
+//  Created by Hanna on 2020/11/14.
+//  Copyright © 2020 鄭佳渝. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
